6ee7976cfcd0279b3e41dba975dc6d515963190b
vishalpatil12014017 <87421912+vishalpatil12014017@users.noreply.github.com>
2021-11-02 14:01:28 +0530

    Footer

--- traveltech/src/components/Home/Footer.jsx
+++ traveltech/src/components/Home/Footer.jsx
@@ -1,4 +1,3 @@
-
 import styled from "styled-components";
 import main from './Logos/main.png'
 import cir from './Logos/cir.png'
@@ -8,102 +7,16 @@ import twitter from './Logos/twitter.png'
 import poper from './Logos/poper.png'
 
 const Toolbar = styled.header`
-padding:20px;
 background-color: #F56753;
-font-family:Noto Serif;
-color: #FFFFFF;
-.grid{
-    display:grid;
-    grid-template-columns:10% 40% 20% 30%;
+.card{
+    background-color: #F56753;
+    border:none   
 }
-img{
-    background-color:#F56753;
-}
-.img{
-    margin-left:10%;
-    /* padding:10px */
-}
-.card-text{
-    //styleName: Body 1;
-font-family: Noto Serif;
-font-size: 1rem;
-font-style: normal;
-font-weight: 400;
-line-height: 1.562rem;
-letter-spacing: 0.01em;
-text-align: left;
-
-}
-.card-ti{
-    font-family: Poppins;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 1.5rem;
-    color: #000000;
-    margin-bottom: 0px;
-    margin-top: 0px;
-}
-.car{
-    margin-top:35px
-}
-.form-control {
-    width:16.93rem;
-    color:#FFFFFF;
-    height:2.5rem;
-    border: 1px solid #FFFFFF;
-    box-sizing: border-box;
-    border-radius: 5px;
-    background-color:#F56753;
-    font-size:16px;
-    margin-top:5px;
-    margin-bottom:30px;
-    
-    
-@media all and (max-width:1060px){
-    width: 12rem;
-   
-}
-}
-.btn{
-height:2.5rem ;
-width: 7.31rem;
-left: 28.81rem;
-top: 36rem;
-border-radius: 5px;
-padding: 0.5rem 1rem 0.5rem 1rem;
-background-color:#262626;
-margin-left:1rem;
-color:white;
-@media all and (max-width:1060px){
-    width: 5.5rem;
-    margin-left:0px
+input{
+    background-color: #F56753; 
 }
 
-}
-.gridnew{
-    display:grid;
-    grid-template-columns:30% 70%;
-    font-family: Noto Serif;
-font-style: normal;
-font-weight: normal;
-font-size: 16px;
-line-height: 25px;
-/* identical to box height, or 156% */
 
-letter-spacing: 0.01em;
-}
-.d-flex{
-    margin-top:40px
-}
-.logo{
-    display:grid;
-    grid-template-columns:60% 40%;
-    margin-top:20px
-}
-.logo1{
-    display:grid;
-    grid-template-columns:25% 25% 25% 25%;
-}
 
 `
 
@@ -111,61 +24,59 @@ function Footer() {
     return (
         <>
             <Toolbar>
-                <div className="fluid-container" >
-                    <div className="img">
-                        <img src={cir} className="p-2" alt="..." />
-                        <img src={main} className="p-2" alt="..." />
+                <div className="fluid-container p-4 " >
+                    <div className="row">
+                        <div className="col col-1"></div>
+                        <div className="col col-4">
+                            <img className="img-fluid" src={cir} className="p-2" alt="..." />
+                            <img className="img-fluid" src={main} className="p-2" alt="..." />
+                        </div>
                     </div>
-
-                    <div className="grid ">
-                        <div>
+                    <div className="row">
+                        <div className="col col-1">
 
                         </div>
-                        <div>
-                            <div className="card mb-3">
+                        <div className="col col-5">
+                            <div className="card" >
                                 <div className="card-body">
-                                    <p className="card-text">We provide campervan rental in destinations across India through a seamless and personalized online experience.</p>
-                                    <p className="card-ti mb-0 car">Subscribe to our newsletter</p>
-
-                                    <p className="card-text">Subscribe to receive occasional updates on top Campervan rental deals, special offers and insightful updates!</p>
-                                </div>
-                                <form className="d-flex">
-                                    <input className="form-control" type="search" placeholder="Enter  your e-mail" aria-label="Search" />
-                                    <button className="btn btn-outline-success" type="submit">Subscribe</button>
-                                </form>
-                            </div>
-                        </div>
-                        <div>
-
-                        </div>
-                        <div>
-                            <p className="card-ti">Navigation</p>
-                            <div className="gridnew">
-                                <div>
-                                    <p>Home</p>
-                                    <p>About us</p>
-                                    <p>Services</p>
-                                    <p>Contact us</p>
-                                </div>
-                                <div>
-                                    <p>Privacy Policy</p>
-                                    <p>Term & Conditions</p>
-                                    <p>Refund & Cancellation policy</p>
-                                    <p>FAQs</p>
+                                    <p className="card-text text-white mb-3">We provide campervan rental in destinations across India through a seamless and personalized online experience.</p>
+                                    <h5 className="card-title mt-4 mb-3 mt-5">Subscribe to our newsletter</h5>
+                                    <p className="card-text text-white mb-5">Subscribe to receive occasional updates on top Campervan rental deals, special offers and insightful updates!</p>
+                                    <form className="d-flex mt-5">
+                                        <input className="form-control  border border-white" type="search" placeholder="Search" aria-label="Search" />
+                                        <button className="btn bg-dark text-white ml-2" type="submit">Subscribe</button>
+                                    </form>
                                 </div>
                             </div>
-                            <div className="logo">
-                                <div className="logo1">
-                                    <div> <img src={faceboot} alt="" /></div>
-                                    <div> <img src={instagram} alt="" /></div>
-                                    <div> <img src={poper} alt="" /></div>
-                                    <div><img src={twitter} alt="" /></div>
+                        </div>
+                        <div className="col col-2"></div>
+                        <div className="col col-4">
+                            <h5 className="card-title">Navigation</h5>
+                            <div className="row mt-0">
+                                <div className="col col-4 col-12  g-3 col-lg-6 col-xl-4">
+                                    <p className="card-text text-white">Home</p>
+                                    <p className="card-text text-white">About us</p>
+                                    <p className="card-text text-white">Services</p>
+                                    <p className="card-text text-white">Contact us</p>
                                 </div>
-                                <div></div>
+                                <div className="col col-8 col-12  g-3 col-lg-6 col-xl-4">
+                                    <p className="card-text text-white">Privacy Policy</p>
+                                    <p className="card-text text-white">Term & Conditions</p>
+                                    <p className="card-text text-white">Refund & Cancellation policy</p>
+                                    <p className="card-text text-white">FAQs</p>
+                                </div>
+                            </div>
+                            <div className="row mt-4">
+                                <div className="col col-2 col-12  g-2 col-sm-2 col-xxl-2"> <img src={faceboot} alt="" /></div>
+                                <div className="col col-2 col-12  g-2 col-sm-2 col-xxl-2"> <img src={instagram} alt="" /></div>
+                                <div className="col col-2 col-12  g-2 col-sm-2 col-xxl-2"> <img src={poper} alt="" /></div>
+                                <div className="col col-2 col-12  g-2 col-sm-2 col-xxl-2"><img src={twitter} alt="" /></div>
+                                <div className="col col-2 col-12  g-2 col-sm-2 col-xxl-2"></div>
+                                <div className="col col-2 col-12  g-2 col-sm-2 col-xxl-2"></div>
                             </div>
                         </div>
-
                     </div>
+
                 </div>
             </Toolbar>
         </>
--- traveltech/src/components/Home/Home.jsx
+++ traveltech/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from './Footer'
+import FooterTop from './FooterTop'
 
 function Home() {
     return (
         <div>
+            {/* <FooterTop/> */}
             <Footer></Footer>
         </div>
     )
